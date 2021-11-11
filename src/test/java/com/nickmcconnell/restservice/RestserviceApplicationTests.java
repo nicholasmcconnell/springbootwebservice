30c7f6c475cb6349df9d24b9c48fda8cf4a4da0e
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2021-11-11 15:31:53 -0600

    first commit

---
+++ src/test/java/com/nickmcconnell/restservice/RestserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nickmcconnell.restservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
